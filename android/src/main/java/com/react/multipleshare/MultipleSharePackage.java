ef695f48fb431beaad133976ee364c34002f4bef
anddju <anddju@126.com>
2019-04-11 17:40:00 +0800

    Update MultipleSharePackage.java
    暴露 createJSModules ， rn0.47一下需要

--- android/src/main/java/com/react/multipleshare/MultipleSharePackage.java
+++ android/src/main/java/com/react/multipleshare/MultipleSharePackage.java
@@ -21,4 +21,9 @@ public class MultipleSharePackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
+    
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 }
