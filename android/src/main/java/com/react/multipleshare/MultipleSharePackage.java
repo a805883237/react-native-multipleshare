cf4e9798fe41cdd88e4695987d70b60b41b463c6
anddju <anddju@126.com>
2019-04-12 20:37:20 +0800

    Update MultipleSharePackage.java

--- android/src/main/java/com/react/multipleshare/MultipleSharePackage.java
+++ android/src/main/java/com/react/multipleshare/MultipleSharePackage.java
@@ -3,6 +3,7 @@ package com.react.multipleshare;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Arrays;
